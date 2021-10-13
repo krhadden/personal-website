529f552ce20b7a9a4b6dc09569da5662edbfe9b5
Kate Hadden <krhadden@gmail.com>
2021-10-12 23:13:13 -0700

    style changes and added custom favicon

--- personal-website/src/app/profile/about/about.component.ts
+++ personal-website/src/app/profile/about/about.component.ts
@@ -9,7 +9,7 @@ export class AboutComponent implements OnInit {
 
   public name = "Hi, I'm Kate!";
   public subtitle = "rock climber, aerialist, software engineer";
-  public bio = "My passion lives where creativity and logic collide.  I love various forms of artistic expression, and thinking logically is at my core.  I earned a Bachelor of Science in Applied Mathematics from the University of Arkansas in 2018 then moved to San Diego, CA to work as a software engineer at BAE Systems, Inc.  The process of creating new, intuitive designs to meet an abstract end goal is such a rewarding task- and it’s what I aim to do in my future.  I’m driven to design technologies that let humans interact with computers in novel ways to improve conventions effectively and creatively. ";
+  public bio = "My passion lives where creativity and logic collide.  I love various forms of artistic expression, from aerial silks to painting to photography (all photos on this site are mine!). Thinking logically is at my core-  I earned a B.S. in Applied Mathematics from the University of Arkansas in 2018 and have been working as a software engineer ever since. In San Diego at BAE Systems, Inc., I created web apps for satellites and crime investigations.  For the past year at Microsoft, I've been creating DevOps monitors for automations and migrating Azure services to a Top Secret cloud environment.  The process of creating new, intuitive designs to meet an abstract end goal is such a rewarding task, and it’s what I aim to do in my future.  I’m driven to design technologies that let humans interact with computers in novel ways and to improve conventions effectively and creatively.";
 
   constructor() { }
 
